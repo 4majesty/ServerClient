989634a62dd5d669b6593882add48d4703ace3d7
Teoge <Teoge@users.noreply.github.com>
2018-01-03 19:24:47 +0800

    add protocol

--- src/vfs/struct/VSFProtocols.java
+++ src/vfs/struct/VSFProtocols.java
@@ -3,5 +3,8 @@ package vfs.struct;
 public class VSFProtocols {
 	public static final int WRITE_CHUNK = 1000;
 	public static final int READ_CHUNK = 1001;
-	public static final int INITIALIZE_CHUNK = 2000;
+	public static final int INITIALIZE_CHUNK_INFO = 2000;
+	public static final int NEW_CHUNK = 2001;
+	public static final int RELEASE_CHUNK = 2002;
+	public static final int HEART_BEAT_DETECT_TO_SLAVE = 2003;
 }
